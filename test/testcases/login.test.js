9f134723ea870264809a78d234bc6d171200ad51
acquaint <acquaint@acquaints-Mac-mini-2.local>
2025-08-18 14:56:07 +0530

    New test cases

--- test/testcases/login.test.js
+++ test/testcases/login.test.js
@@ -7,25 +7,63 @@ describe('Login page test cases', () => {
     let driver;
     let loginPage;
 
-    before(async () =>{
+   /* before(async () =>{
         driver = await getDriver();
         loginPage = new LoginPage(driver);
         await loginPage.open();
     });
+
+    beforeEach(async () => {
+    await driver.manage().deleteAllCookies();   // clear session
+    await loginPage.open()
+    });
+
     after(async() => {
         await quitDriver(driver);
-    });
-    beforeEach(async() => {
+    }); 
+    */
+   beforeEach(async () =>{
+        driver = await getDriver();
+        loginPage = new LoginPage(driver);
         await loginPage.open();
     });
-    it('Login with valid credentials', async() =>{
+    afterEach(async() => {
+        await quitDriver(driver);
+    }); 
+    it('Login with valid credentials', async() => {
         await loginPage.changeToEmail();
         await loginPage.enterUsername(process.env.VALID_USERNAME);
         await loginPage.enterPassword(process.env.VALID_PASSWORD);
         await loginPage.clickLogin();
         const currentUrl = await driver.getCurrentUrl();
         expect(currentUrl.includes('/dashboard')).to.be.true;
-
     
+    });
+    it('Login with invalid credentials', async() => {
+        await loginPage.changeToEmail();
+        await loginPage.enterUsername(process.env.INVALID_USERNAME);
+        await loginPage.enterPassword(process.env.INVALID_PASSWORD);
+        await loginPage.clickLogin();
+        //const currentUrl = await driver.getCurrentUrl();
+        //expect(currentUrl.includes('/dashboard')).to.be.false; 
+        const errorMessage = await loginPage.getErrorMessage();
+        console.log(errorMessage);
+        expect(errorMessage).to.match(/Thông tin Đăng nhập không hợp lệ/);
+    });
+    it('Login with valid credentials using mobile number', async() => {
+        await loginPage.selectCountryCode();
+        await loginPage.enterMobileNumber(process.env.VALID_MNUMBER);
+        await loginPage.enterPassword(process.env.VALID_PASSWORD);
+        await loginPage.clickLogin();
+        const currentUrl = await driver.getCurrentUrl();
+        expect(currentUrl.includes('/dashboard')).to.be.true;
     })
-})
+    it('Login with Invalid mobile number', async() => {
+        await loginPage.selectCountryCode();
+        await loginPage.enterMobileNumber(process.env.INVALID_MNUMBER);
+        await loginPage.enterPassword(process.env.INVALID_PASSWORD);
+        await loginPage.clickLogin();
+        const errorMessage = await loginPage.getErrorMessage();
+        expect(errorMessage).to.match(/Thông tin Đăng nhập không hợp lệ/);
+    });
+});
